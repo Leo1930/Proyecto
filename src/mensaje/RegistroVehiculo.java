465aee2a35a2228c1c8a7b042c229330a78fb098
kleberjaneta <89557026+kleberjaneta@users.noreply.github.com>
2021-09-11 22:17:39 -0500

    Update RegistroVehiculo.java

--- src/mensaje/RegistroVehiculo.java
+++ src/mensaje/RegistroVehiculo.java
@@ -11,7 +11,72 @@ package mensaje;
  */
 public class RegistroVehiculo {
     
+   protected String tipovehiculo;
+    private String marca;
+    private String modelo;
+    private String placa;
+    private int aniofabricacion;
+    ArrayList<RegistroCliente> cliente;
+    
    public RegistroVehiculo(){
    }
+   public RegistroVehiculo(String tipovehiculo,String marca,String modelo,String placa
+                            ,int aniofabricacion){
+       this.tipovehiculo=tipovehiculo;
+       this.marca=marca;
+       this.modelo=modelo;
+       this.placa=placa;
+       this.aniofabricacion=aniofabricacion;
+       cliente=new ArrayList<>();
+   }
+
+    public String getTipovehiculo() {
+        return tipovehiculo;
+    }
+
+    public void setTipovehiculo(String tipovehiculo) {
+        this.tipovehiculo = tipovehiculo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAniofabricacion() {
+        return aniofabricacion;
+    }
+
+    public void setAniofabricacion(int aniofabricacion) {
+        this.aniofabricacion = aniofabricacion;
+    }
+
+    public ArrayList<RegistroCliente> getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(ArrayList<RegistroCliente> cliente) {
+        this.cliente = cliente;
+    }
+    //este tambien vele si sirve jaja
     
 }
